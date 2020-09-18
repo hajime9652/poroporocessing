f963e149008d2901175e53c1bb2f3aa959f69d89
Hajime Hatano <hajime9652@gmail.com>
2020-09-18 23:02:52 +0900

    refactor

--- home/sketch.js
+++ home/sketch.js
@@ -1,80 +1,30 @@
-//breakout close (core mechanics)
-//mouse to control the paddle, click to start
+//Creating animations
 
-var paddle, ball, wallTop, wallBottom, wallLeft, wallRight;
-var bricks;
-var MAX_SPEED = 9;
-var WALL_THICKNESS = 30;
-var BRICK_W = 40;
-var BRICK_H = 20;
-var BRICK_MARGIN = 4;
-var ROWS = 9;
-var COLUMNS = 16;
+//animations like p5 images should be stored in variables
+//in order to be displayed during the draw cycle
+var ghost, asterisk;
+
+//it's advisable (but not necessary) to load the images in the preload function
+//of your sketch otherwise they may appear with a little delay
+function preload() {
+
+  //create an animation from a sequence of numbered images
+  //pass the first and the last file name and it will try to find the ones in between
+  ghost = loadAnimation('assets/ghost_standing0001.png', 'assets/ghost_standing0007.png');
+
+  //create an animation listing all the images files
+  // asterisk = loadAnimation('assets/asterisk.png', 'assets/triangle.png', 'assets/square.png', 'assets/cloud.png', 'assets/star.png', 'assets/mess.png', 'assets/monster.png');
+}
 
 function setup() {
-  createCanvas(800, 600);
-
-  paddle = createSprite(width / 2, height - 50, 100, 10);
-  paddle.immovable = true;
-
-  wallTop = createSprite(width / 2, -WALL_THICKNESS / 2, width + WALL_THICKNESS * 2, WALL_THICKNESS);
-  wallTop.immovable = true;
-
-  wallBottom = createSprite(width / 2, height + WALL_THICKNESS / 2, width + WALL_THICKNESS * 2, WALL_THICKNESS);
-  wallBottom.immovable = true;
-
-  wallLeft = createSprite(-WALL_THICKNESS / 2, height / 2, WALL_THICKNESS, height);
-  wallLeft.immovable = true;
-
-  wallRight = createSprite(width + WALL_THICKNESS / 2, height / 2, WALL_THICKNESS, height);
-  wallRight.immovable = true;
-
-  bricks = new Group();
-
-  var offsetX = width / 2 - (COLUMNS - 1) * (BRICK_MARGIN + BRICK_W) / 2;
-  var offsetY = 80;
-
-  for (var r = 0; r < ROWS; r++)
-    for (var c = 0; c < COLUMNS; c++) {
-      var brick = createSprite(offsetX + c * (BRICK_W + BRICK_MARGIN), offsetY + r * (BRICK_H + BRICK_MARGIN), BRICK_W, BRICK_H);
-      brick.shapeColor = color(255, 255, 255);
-      bricks.add(brick);
-      brick.immovable = true;
-    }
-
-  //the easiest way to avoid pesky multiple collision is to
-  //have the ball bigger than the bricks
-  ball = createSprite(width / 2, height - 200, 11, 11);
-  ball.maxSpeed = MAX_SPEED;
-  paddle.shapeColor = ball.shapeColor = color(255, 255, 255);
-
+  createCanvas(800, 300);
 }
 
 function draw() {
-  background(247, 134, 131);
+  background(255, 255, 255);
 
-  paddle.position.x = constrain(mouseX, paddle.width / 2, width - paddle.width / 2);
-
-  ball.bounce(wallTop);
-  ball.bounce(wallBottom);
-  ball.bounce(wallLeft);
-  ball.bounce(wallRight);
-
-  if (ball.bounce(paddle)) {
-    var swing = (ball.position.x - paddle.position.x) / 3;
-    ball.setSpeed(MAX_SPEED, ball.getDirection() + swing);
-  }
-
-  ball.bounce(bricks, brickHit);
-
-  drawSprites();
-}
-
-function mousePressed() {
-  if (ball.velocity.x == 0 && ball.velocity.y == 0)
-    ball.setSpeed(MAX_SPEED, random(90 - 10, 90 + 10));
-}
-
-function brickHit(ball, brick) {
-  brick.remove();
+  //specify the animation instance and its x,y position
+  //animation() will update the animation frame as well
+  animation(ghost, 300, 150);
+  // animation(asterisk, 500, 150);
 }
