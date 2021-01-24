72802429acc0ee93f8e8f74b0d8445776f32f51a
Hajime Hatano <hajime9652@gmail.com>
2021-01-24 23:03:18 +0000

    add p5 and ml5

---
+++ ml/src/components/Interface.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <Movie />
+        <Wave />
+        <Image />
+        <Lang />
+    </div>
+</template>
+
+<script>
+import Movie from './components/Movie.vue'
+import Wave from './components/Wave.vue'
+import Image from './components/Image.vue'
+import Lang from './components/Lang.vue'
+
+export default {
+    name: 'Interface',
+    component: {
+        Movie,
+        Wave,
+        Image,
+        Lang,
+    }
+}
+</script>
